643d951e92dd0f8189bbcf502d560a5b0e3777bb
farooquiumar27 <farooquiumar27@gmail.com>
2022-08-18 05:22:09 +0530

    Part-2

---
+++ Program for cin and cout/eg2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Enter number: ";
+int x;
+cin>>x;
+cout<<"Enter another number: ";
+int y;
+cin>>y;
+int z;
+z=x+y;
+cout<<"The total is  "<<z;
+return 0; 
+}
---
+++ Program for cin and cout/eg3/problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+cout<<"Enter number: ";
+int x;
+cin>>x;
+cout<<"Enter another number: ";
+int y;
+cin>>y;
+int z;
+z=x+y;
+cout<<"The value of x is "<<x;
+cout<<"The value of y is "<<y;
+cout<<"The total is "<<z;
+return 0;
+}
